2f474e47cf725a4708f6fb06276bbfb472eca635
Felix Jendrusch <felix@felixjendrusch.is>
2017-01-06 09:51:52 +0100

    Rework interaction and value recording
    - `TimestampRecording` has been renamed to `InteractionRecording`,
      which now provides access to more than just the timestamp. It also
      includes a textual representation of each interaction as well as the
      file and line of occurrence.
    - `IntractionRecording` has been renamed to `ValueRecording`, providing
      chronological access to recorded interactions and their corresponding
      values.
    - The global logical clock is now thread-safe.
    - The recorder is now thread-safe.

--- Dobby/Recorder.swift
+++ Dobby/Recorder.swift
@@ -1,8 +1,9 @@
 /// A timestamp in a logical clock.
 public typealias Timestamp = UInt64
 
-private var timestamp: Timestamp = 0
-private let timestampQueue = DispatchQueue(label: "com.trivago.dobby-timestampQueue", attributes: .concurrent)
+/// A thread-safe global logical clock.
+fileprivate var timestamp: Timestamp = 0
+fileprivate let timestampQueue = DispatchQueue(label: "com.trivago.dobby-timestampQueue", attributes: .concurrent)
 
 /// Returns the current timestamp of the global clock.
 public var currentTimestamp: Timestamp {
@@ -19,30 +20,24 @@ public func nextTimestamp() -> Timestamp {
     })
 }
 
-/// A type that provides chronological access to recorded timestamps.
-public protocol TimestampRecording: class {
-    /// Returns the recorded timestamps in chronological order.
-    var timestamps: [Timestamp] { get }
-}
+/// A recorded interaction.
+public struct Interaction: CustomStringConvertible {
+    public let description: String
 
-/// An interaction, consisting of a timestamp and a value.
-public struct Interaction<Value> {
-    /// The time when the interaction occurred.
+    /// The time when this interaction was recorded.
     public let timestamp: Timestamp
 
-    /// The value of the interaction.
-    public let value: Value
-
-    /// The file in which the interaction occurred.
+    /// The file in which this interaction was recorded.
     public let file: StaticString
 
-    /// The line at which the interaction occurred.
+    /// The line at which this interaction was recorded.
     public let line: UInt
 
-    /// Creates a new interaction with the given value at the specified time.
-    public init(value: Value, timestamp: Timestamp, file: StaticString, line: UInt) {
+    /// Creates a new interaction with the given textual representation at the
+    /// specified time.
+    public init(description: String, timestamp: Timestamp, file: StaticString, line: UInt) {
+        self.description = description
         self.timestamp = timestamp
-        self.value = value
 
         self.file = file
         self.line = line
@@ -50,44 +45,123 @@ public struct Interaction<Value> {
 }
 
 /// A type that provides chronological access to recorded interactions.
-public protocol InteractionRecording: TimestampRecording {
-    /// The value type of recorded interactions.
+public protocol InteractionRecording: class {
+    /// Returns the unique identifier for this object.
+    var objectIdentifier: ObjectIdentifier { get }
+
+    /// Returns a random access collection that provides access to the recorded
+    /// interactions in chronological order.
+    var interactions: AnyRandomAccessCollection<Interaction> { get }
+}
+
+extension InteractionRecording {
+    public var objectIdentifier: ObjectIdentifier {
+        return ObjectIdentifier(self)
+    }
+}
+
+/// A type-erased, hashable interaction recorder. The hash value and equality
+/// operator are implemented using object identifiers.
+internal final class AnyInteractionRecording: InteractionRecording, Hashable {
+    /// The wrapped interaction recorder.
+    private let base: InteractionRecording
+
+    internal var objectIdentifier: ObjectIdentifier {
+        return base.objectIdentifier
+    }
+
+    internal var interactions: AnyRandomAccessCollection<Interaction> {
+        return base.interactions
+    }
+
+    internal var hashValue: Int {
+        return objectIdentifier.hashValue
+    }
+
+    /// Creates a type-erased, hashable interaction recorder that wraps the
+    /// given instance.
+    internal init(_ base: InteractionRecording) {
+        self.base = base
+    }
+}
+
+internal func == (lhs: AnyInteractionRecording, rhs: AnyInteractionRecording) -> Bool {
+    return lhs.objectIdentifier == rhs.objectIdentifier
+}
+
+/// A type that provides chronological access to recorded interactions and
+/// corresponding values.
+public protocol ValueRecording: InteractionRecording {
+    /// The type of recorded values.
     associatedtype Value
 
-    /// Returns the recorded interactions in chronological order.
-    var interactions: [Interaction<Value>] { get }
+    /// Returns the value corresponding to the interaction at the given index.
+    func valueForInteraction(at index: Int) -> Value
 }
 
-public extension InteractionRecording {
-    public var timestamps: [Timestamp] {
-        return interactions.map({ interaction in
-            return interaction.timestamp
-        })
+/// A recorded entry.
+fileprivate struct Entry<Value> {
+    /// The recorded interaction.
+    fileprivate let interaction: Interaction
+
+    /// The recorded value.
+    fileprivate let value: Value
+
+    /// Creates a new entry with the given interaction and value.
+    fileprivate init(interaction: Interaction, value: Value) {
+        self.interaction = interaction
+        self.value = value
+    }
+
+    /// Creates a new entry using the value's textual representation as
+    /// description for the interaction.
+    fileprivate init(value: Value, timestamp: Timestamp, file: StaticString, line: UInt) {
+        let interaction = Interaction(description: String(describing: value), timestamp: timestamp, file: file, line: line)
+
+        self.init(interaction: interaction, value: value)
     }
 }
 
-/// A recorder for interactions of the specified value type.
-public final class Recorder<Value>: InteractionRecording {
-    private var _interactions: [Interaction<Value>] = []
-    private let _interactionsQueue = DispatchQueue(label: "com.trivago.dobby.recorder-interactionsQueue", attributes: .concurrent)
-
-    public var interactions: [Interaction<Value>] {
-        return _interactionsQueue.sync(execute: {
-            return _interactions
-        })
-    }
+/// A thread-safe recorder for interactions of the specified value type.
+public final class Recorder<Value> {
+    fileprivate var entries: [Entry<Value>] = []
+    fileprivate let entriesQueue = DispatchQueue(label: "com.trivago.dobby.recorder-entriesQueue", attributes: .concurrent)
 
     /// Creates a new recorder.
     public init() {
 
     }
 
-    /// Records an interaction with the given value at the current timestamp of
-    /// the global clock.
+    /// Records an interaction with the given value.
     public func record(_ value: Value, file: StaticString = #file, line: UInt = #line) {
-        _interactionsQueue.sync(flags: .barrier, execute: {
-            let interaction = Interaction(value: value, timestamp: nextTimestamp(), file: file, line: line)
-            _interactions.append(interaction)
+        entriesQueue.sync(flags: .barrier, execute: {
+            // Get the next timestamp while executing on the entries queue to
+            // guarantee that entries are appended in chronological order.
+            let entry = Entry(value: value, timestamp: nextTimestamp(), file: file, line: line)
+
+            entries.append(entry)
         })
+    }
+}
+
+extension Recorder: ValueRecording {
+    public var interactions: AnyRandomAccessCollection<Interaction> {
+        let entries = entriesQueue.sync(execute: {
+            return self.entries
+        })
+
+        let interactions = entries.lazy.map({ entry in
+            return entry.interaction
+        })
+
+        return AnyRandomAccessCollection(interactions)
+    }
+
+    public func valueForInteraction(at index: Int) -> Value {
+        let entries = entriesQueue.sync(execute: {
+            return self.entries
+        })
+
+        return entries[index].value
     }
 }
--- DobbyTests/RecorderSpec.swift
+++ DobbyTests/RecorderSpec.swift
@@ -12,17 +12,19 @@ class RecorderSpec: QuickSpec {
         }
 
         describe("Recording") {
-            it("appends an interaction to the recorder") {
+            it("appends an interaction and its corresponding value to the recorder") {
                 let currentTimestamp = Dobby.currentTimestamp
 
                 recorder.record(1)
 
-                // Verify that the interaction has been appended.
+                // Verify that the interaction has been recorded.
                 expect(recorder.interactions).to(haveCount(1))
-                expect(recorder.interactions[0].timestamp).to(equal(currentTimestamp))
-                expect(recorder.interactions[0].value).to(equal(1))
+                expect(recorder.interactions[AnyIndex(0)].timestamp).to(equal(currentTimestamp + 1))
 
-                // Verify that the global clock is advanced.
+                // Verify that the corresponding value has been recorded.
+                expect(recorder.valueForInteraction(at: 0)).to(equal(1))
+
+                // Verify that the global clock has been advanced.
                 expect(Dobby.currentTimestamp).to(equal(currentTimestamp + 1))
             }
         }
