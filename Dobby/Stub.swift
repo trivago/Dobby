3d0238a807e719d6ac483a902a298a631de450ad
Felix Jendrusch <felix@felixjendrusch.is>
2017-01-06 10:26:08 +0100

    Rename stub behaviors to reactions

--- Dobby/Stub.swift
+++ Dobby/Stub.swift
@@ -1,89 +1,89 @@
-/// A matcher-based behavior with closure-based handling.
-fileprivate struct Behavior<Interaction, ReturnValue> {
-    /// The matcher of this behavior.
-    fileprivate let matcher: Matcher<Interaction>
+/// A matcher-based reaction with closure-based handling.
+fileprivate struct Reaction<Value, ReturnValue> {
+    /// The matcher of this reaction.
+    fileprivate let matcher: Matcher<Value>
 
-    /// The handler of this behavior.
-    fileprivate let handler: (Interaction) -> ReturnValue
+    /// The handler of this reaction.
+    fileprivate let handler: (Value) -> ReturnValue
 
-    /// Initializes a new behavior with the given matcher and handler.
-    fileprivate init<M: MatcherConvertible>(matcher: M, handler: @escaping (Interaction) -> ReturnValue) where M.ValueType == Interaction {
+    /// Initializes a new reaction with the given matcher and handler.
+    fileprivate init<Matcher: MatcherConvertible>(matcher: Matcher, handler: @escaping (Value) -> ReturnValue) where Matcher.ValueType == Value {
         self.matcher = matcher.matcher()
         self.handler = handler
     }
 }
 
 /// A stub error.
-public enum StubError<Interaction, ReturnValue>: Error {
-    /// The associated interaction was unexpected.
-    case unexpectedInteraction(Interaction)
+public enum StubError<Value, ReturnValue>: Error {
+    /// An interaction with the associated value was unexpected.
+    case unexpectedInteraction(Value)
 }
 
-/// A stub that, when invoked, returns a value based on the set up behavior, or,
-/// if an interaction is unexpected, throws an error.
-public final class Stub<Interaction, ReturnValue> {
-    /// The current (next) identifier for behaviors.
+/// A stub that, when invoked, returns a value based on the set up reactions,
+/// or, if an interaction is unexpected, throws an error.
+public final class Stub<Value, ReturnValue> {
+    /// The current (next) identifier for reactions.
     private var currentIdentifier: UInt = 0
 
-    /// The behaviors of this stub.
-    private var behaviors: [(identifier: UInt, behavior: Behavior<Interaction, ReturnValue>)] = []
+    /// The reactions of this stub.
+    private var reactions: [(identifier: UInt, reaction: Reaction<Value, ReturnValue>)] = []
 
-    /// Initializes a new stub.
+    /// Creates a new stub.
     public init() {
         
     }
 
-    /// Modifies the behavior of this stub, forwarding invocations to the given
-    /// function and returning its return value if the given matcher does match
-    /// an interaction.
+    /// Modifies the reactions of this stub, forwarding invocations to the
+    /// given handler and returning its return value if the given matcher
+    /// does match an interaction.
     ///
-    /// Returns a disposable that, when disposed, removes this behavior.
+    /// Returns a disposable that, when disposed, removes this reaction.
     @discardableResult
-    public func on<M: MatcherConvertible>(_ matcher: M, invoke handler: @escaping (Interaction) -> ReturnValue) -> Disposable where M.ValueType == Interaction {
+    public func on<Matcher: MatcherConvertible>(_ matcher: Matcher, invoke handler: @escaping (Value) -> ReturnValue) -> Disposable where Matcher.ValueType == Value {
         currentIdentifier += 1
 
         let identifier = currentIdentifier
-        let behavior = Behavior(matcher: matcher, handler: handler)
-        behaviors.append((identifier: identifier, behavior: behavior))
+        let reaction = Reaction(matcher: matcher, handler: handler)
+        reactions.append((identifier: identifier, reaction: reaction))
 
         return Disposable { [weak self] in
-            let index = self?.behaviors.index { otherIdentifier, _ in
+            let index = self?.reactions.index { otherIdentifier, _ in
                 return otherIdentifier == identifier
             }
 
             if let index = index {
-                self?.behaviors.remove(at: index)
+                self?.reactions.remove(at: index)
             }
         }
     }
 
-    /// Modifies the behavior of this stub, returning the given value upon
+    /// Modifies the reactions of this stub, returning the given value upon
     /// invocation if the given matcher does match an interaction.
     ///
-    /// Returns a disposable that, when disposed, removes this behavior.
+    /// Returns a disposable that, when disposed, removes this reaction.
     ///
-    /// - SeeAlso: `Stub.on<M>(matcher: M, invoke: Interaction -> ReturnValue) -> Disposable`
+    /// - SeeAlso: `Stub.on<Matcher>(matcher: Matcher, invoke: (Value) -> ReturnValue) -> Disposable`
     @discardableResult
-    public func on<M: MatcherConvertible>(_ matcher: M, return value: ReturnValue) -> Disposable where M.ValueType == Interaction {
+    public func on<Matcher: MatcherConvertible>(_ matcher: Matcher, return value: ReturnValue) -> Disposable where Matcher.ValueType == Value {
         return on(matcher) { _ in value }
     }
 
-    /// Invokes this stub, returning a value based on the set up behavior, or,
+    /// Invokes this stub, returning a value based on the set up reactions, or,
     /// if the given interaction is unexpected, throwing an error.
     ///
-    /// Behavior is matched in order, i.e., the function associated with the
+    /// Reactions are matched in order, i.e., the handler associated with the
     /// first matcher that matches the given interaction is invoked.
     ///
-    /// - Throws: `StubError.unexpectedInteraction(Interaction)` if the given
+    /// - Throws: `StubError.unexpectedInteraction(Value)` if the given
     ///     interaction is unexpected.
     @discardableResult
-    public func invoke(_ interaction: Interaction) throws -> ReturnValue {
-        for (_, behavior) in behaviors {
-            if behavior.matcher.matches(interaction) {
-                return behavior.handler(interaction)
+    public func invoke(_ value: Value) throws -> ReturnValue {
+        for (_, reaction) in reactions {
+            if reaction.matcher.matches(value) {
+                return reaction.handler(value)
             }
         }
 
-        throw StubError<Interaction, ReturnValue>.unexpectedInteraction(interaction)
+        throw StubError<Value, ReturnValue>.unexpectedInteraction(value)
     }
 }
